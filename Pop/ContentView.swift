48be264592c3574106489827cdceb88838ced7f3
zmeriksen <zmeriksen@icloud.com>
2020-10-16 21:14:04 -0500

    Initial Commit

---
+++ Pop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pop
+//
+//  Created by Zach Eriksen on 10/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pop/PopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopApp.swift
+//  Pop
+//
+//  Created by Zach Eriksen on 10/16/20.
+//
+
+import SwiftUI
+
+@main
+struct PopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
